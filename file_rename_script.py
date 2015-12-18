5d5cba407ea92d4f1b4f994151460cdb117b1e43
Nicholas Land <nland@authen2cate.com>
2015-12-17 22:36:11 -0500

    The script is working now

--- file_rename_script.py
+++ file_rename_script.py
@@ -1,3 +1,5 @@
+#!/usr/local/opt/coreutils/libexec/gnubin/env python3
+
 import os, time
 from datetime import datetime
 
@@ -6,7 +8,6 @@ files = [f for f in os.listdir(os.getcwd()) if os.path.isfile(f)]
 
 for file in files:
     file_name, file_extension = os.path.splitext(file)
-    print("Name of file:", file)
     create_date = datetime.fromtimestamp(os.path.getctime(file)).strftime('%Y_%m_%d')
     prompt_user = "What would you like to name " + file + " to? "
     user_response = input(prompt_user)
